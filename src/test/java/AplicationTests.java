7aa4b4978dff89cc6ad5014f4aae825a776745c1
vvaranytsa <vanya.varanytsa@gmail.com>
2022-12-11 21:44:14 +0200

    testsss

--- src/test/java/AplicationTests.java
+++ src/test/java/AplicationTests.java
@@ -4,8 +4,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
